5923d955de7cd3bc19d5ae248dfa87b4c8ba73e4
= <=>
2018-10-03 22:12:07 +0530

    Signup

---
+++ app/src/main/java/com/example/gisilk/onlineorder2/com/example/signUp/ResetPasswordActivity.java
@@ -0,0 +1,15 @@
+package com.example.gisilk.onlineorder2.com.example.signUp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gisilk.onlineorder2.R;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
